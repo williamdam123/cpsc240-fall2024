61fa11569909ccd0aefea6c845ebe52cb22534f0
williamdam123 <143433814+williamdam123@users.noreply.github.com>
2024-12-16 15:42:29 -0800

    Update strlen.asm

--- hw6/strlen.asm
+++ hw6/strlen.asm
@@ -1,12 +1,13 @@
+; strlen.asm
 ; Program name: "Electricity Calculator". Calculate string length.
 ; Author: William Dam
 
 global strlen
 section .text
 strlen:
-    mov rcx, -1
-    xor al, al
-    repnz scasb
-    not rcx
-    dec rcx
+    mov rcx, -1            ; Set RCX to -1 for the loop
+    xor al, al             ; Set AL to 0 (null character)
+    repnz scasb            ; Scan the string for null character
+    not rcx                ; Invert RCX to get the length
+    dec rcx                ; Decrement to exclude null terminator
     ret
