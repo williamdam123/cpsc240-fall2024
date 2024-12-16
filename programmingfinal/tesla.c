57e81912d0f2aff4fdfbda7332115610b53b3e98
williamdam123 <143433814+williamdam123@users.noreply.github.com>
2024-12-16 08:35:33 -0800

    Update tesla.c

--- programmingfinal/tesla.c
+++ programmingfinal/tesla.c
@@ -1,3 +1,28 @@
+// Program name: "Electric Current Calculator". This program calculates the current 
+// in a circuit with two parallel sub-circuits using modular components.
+// Copyright (C) 2024 William Dam
+
+// This file is part of the software program "Electric Current Calculator".
+
+// Electric Current Calculator is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Electric Current Calculator is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>. 
+
+// Author information
+//   Author name : William Dam
+//   Author email: wdam1@csu.fullerton.edu
+//   Author section: 240-19
+//   Author CWID : 884616384
+
 #include <stdio.h>
 
 // Declaration of electricity function from edison.asm
