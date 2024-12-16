8460142e63e204f638eb8ce1d11c4d14e03318d8
williamdam123 <143433814+williamdam123@users.noreply.github.com>
2024-12-16 14:46:16 -0800

    Create atof.asm

---
+++ hw6/atof.asm
@@ -0,0 +1,9 @@
+; Program name: "Electricity Calculator". Convert ASCII to float.
+; Author: William Dam
+
+global atof
+section .text
+atof:
+    ; Placeholder for ASCII to float conversion
+    ; Add implementation or call library if needed
+    ret
